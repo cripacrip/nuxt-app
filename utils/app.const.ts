129d94c2fc9cffaddc9a606f22e2bd53dc5e57a0
Dany <lysakdanil@gmail.com>
2024-12-12 06:57:20 +0200

    feat: add utils

---
+++ utils/app.const.ts
@@ -0,0 +1,8 @@
+// 67544aef003db811a82f
+
+export const APP_WRITE_ID = '67544aef003db811a82f'
+export const DB_ID = 'crm-base'
+export const COLLECTION_DEALS = 'deals'
+export const COLLECTION_CUSTOMERS = 'customers'
+export const COLLECTION_COMMENTS = 'comments';
+export const STORAGE_ID = 'storage'
